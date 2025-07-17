cc1b8e1433fbe681b55eb4125406494378c8afa7
VictorMeimaris <victor_meimaris@outlook.com>
2025-07-17 20:37:44 +0300

    Created file architecture. Will decide with collaborators on master header functions and variables needed

--- inc/dense.hpp
+++ inc/dense.hpp
@@ -11,5 +11,10 @@
  * @brief A class to create a dense matrix object of any number of dimensions.
  */
 class matrix{
-
-}
+    private:
+        /**
+         * @brief Flat vector storing all matrix values.
+         * Αll set to 0.0 by default.
+         */
+        std::vector<double> values;
+    }
