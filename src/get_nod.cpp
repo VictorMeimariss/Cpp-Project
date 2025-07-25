dd5f0725a77e22e0a86350314f46662368513223
johnzik <zgian04@gmail.com>
2025-07-25 11:17:52 +0300

    Added get_nod, set_value_at

---
+++ src/get_nod.cpp
@@ -0,0 +1,10 @@
+#include <./inc/dense.hpp>
+using namespace std;
+
+/**
+ * @brief Gets number of dimensions.
+ */
+size_t matrix::get_nod() const
+{
+    return dim_size.size();
+}
---
+++ src/set_value_at.cpp
@@ -0,0 +1,12 @@
+#include <./inc/dense.hpp>
+using namespace std;
+
+/**
+ * @brief Sets value at a given index.
+ * @param index The linear 0-based index in the flat values vector.
+ * @param value The long double value to set.
+ */
+void matrix::set_value_at(size_t index, long double value)
+{
+    values.at(index) = value;
+}
