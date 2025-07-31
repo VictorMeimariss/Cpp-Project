b51ca5b791831cc428ab1c41ee576d93ff6508ef
VictorMeimaris <victor_meimaris@outlook.com>
2025-07-31 13:50:25 +0300

    updated operators

---
+++ src/complex_operator.cpp
@@ -0,0 +1,8 @@
+#include "dense.hpp"
+template<>
+matrix<std::complex<float>> matrix<std::complex<float>>:: operator+(matrix<std::complex<float>> const& A){
+    matrix<std::complex<float>> B = *this;
+    std::complex<float> a(1, 0);
+    cblas_caxpy(static_cast<int>(A.values.size()), &a, A.values.data(), 1, B.values.data(), 1);
+    return B;
+}
