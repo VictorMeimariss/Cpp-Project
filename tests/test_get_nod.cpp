140179b6a76a5109a5c93a51a1b33776f7b9dc3d
johnzik <zgian04@gmail.com>
2025-08-02 14:30:19 +0000

    Updated test_get_nod

--- tests/test_get_nod.cpp
+++ tests/test_get_nod.cpp
@@ -6,5 +6,6 @@ int main()
 {
     matrix<int> M(4, 2, 2);
 
+    std::cout << "Expected Matrix dimensions = 3" << std::endl; 
     std::cout << "Matrix dimensions = " << M.get_nod() << std::endl;
 }
