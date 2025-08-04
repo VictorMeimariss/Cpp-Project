b1019b7bcfda518e1c368ded135aa959b2bff5ef
VictorMeimaris <victor_meimaris@outlook.com>
2025-08-04 10:27:45 +0300

    Fixed copy constructor, =, set_values at added number * matrix and test

--- tests/test_matrix_scalar_mul.cpp
+++ tests/test_matrix_scalar_mul.cpp
@@ -9,9 +9,20 @@ int main() {
 
     matrix<std::complex<double>> C = A * cd(2, 0);
     
-    std::cout << "Test Expected Answer:\n" << "[(2,2); (7,2)]\n\n" << "Test Result:" << std::endl;
+    std::cout << "Test 1 Expected Answer:\n" << "[(2,2); (7,2)]\n\n" << "Test Result:" << std::endl;
     C.print2D();
     std::cout << "A(expected [(1,1); (3.5,1)]):"<<std::endl;
     A.print2D();
+
+    matrix<double> B(2,1);
+    B.set_value_at(0, 1);
+    B.set_value_at(1, 2);
+
+    matrix<double> D = B * 2;
+    
+    std::cout << "Test 2 Expected Answer:\n" << "[2; 4]\n\n" << "Test Result:" << std::endl;
+    D.print2D();
+    std::cout << "B(expected [1; 2]):"<<std::endl;
+    B.print2D();
     return 0;
 }
