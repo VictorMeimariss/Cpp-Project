2016b85324ae179b293f28cb6ca1c15add6af17e
Athanasios-Mitroulakis <athanmit04@gmail.com>
2025-08-10 00:34:19 +0300

    Update move-constructor.cpp

--- src/move-constructor.cpp
+++ src/move-constructor.cpp
@@ -1,3 +1,1 @@
-#include "../inc/dense.hpp"
-matrix<T>::matrix (matrix<T>&& A) noexcept
-    : values(std::move(A.values)), dim_size(std::move(A.dim_size)), precomputed_strides(std::move(A.precomputed_strides)) {}
+
