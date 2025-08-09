0f8c7e8f39dacf70118f19212b37a0afcafe1d03
Athanasios-Mitroulakis <athanmit04@gmail.com>
2025-08-10 00:37:12 +0300

    Delete src/set_values.cpp

--- src/set_values.cpp
+++
@@ -1,9 +0,0 @@
-#include "../inc/dense.hpp"
-template<typename T>
-template<typename... Types>
-void matrix<T>::set_values(Types... valuess){
-    values = std::forward<Types>(valuess);
-}
-template void matrix<float>::set_values()
-//τελικα στο header
-
