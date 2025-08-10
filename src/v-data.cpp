87d803f9543bc8f99e92db6fb4d3686a172e5cea
VictorMeimaris <victor_meimaris@outlook.com>
2025-08-10 11:41:27 +0300

    Merged Thanasis branch to main

--- src/v-data.cpp
+++
@@ -1,9 +0,0 @@
-#include "../inc/dense.hpp"
-template<typename T>
-T* matrix<T>::v_data(){
-    return values.data();
-}
-template float* matrix<float>::v_data();
-template double* matrix<double>::v_data();
-template std::complex<float>* matrix<std::complex<float>>::v_data();
-template std::complex<double>* matrix<std::complex<double>>::v_data();
