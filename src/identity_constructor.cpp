274bb16b4589698a03dc7566ddc1b28b689da47b
Athanasios-Mitroulakis <athanmit04@gmail.com>
2025-08-28 22:47:56 +0300

    Update identity_constructor.cpp

--- src/identity_constructor.cpp
+++ src/identity_constructor.cpp
@@ -1,18 +1,13 @@
-/*#include "../inc/dense.hpp"
+#include "../inc/dense.hpp"
 template<typename T>
 matrix<T> matrix<T>::I(int N){
-    matrix<T> res;
-    res.dim_size = std::vector<int>(2,N);
-    res.precomputed_strides = std::vector<int>(2);
-    res.values = std::vector<T>(N*N,0);
+    matrix<T> res(N,N);
     for (int i=0; i<N;i++){
-        res.values[i*N+i] = static_cast<T>(1);
+    res.get_values_data()[i*N+i] = static_cast<T>(1);
     }
-    res.precomputed_strides[0] = 1;
-    res.precomputed_strides[1] = N;
     return res;
 }
 template matrix<float> matrix<float>::I(int);
 template matrix<double> matrix<double>::I(int);
 template matrix<std::complex<float>> matrix<std::complex<float>>::I(int);
-template matrix<std::complex<double>> matrix<std::complex<double>>::I(int);*/
+template matrix<std::complex<double>> matrix<std::complex<double>>::I(int);
