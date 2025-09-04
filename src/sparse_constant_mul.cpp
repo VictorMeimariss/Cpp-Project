250bde05a9721a491a69388ec1816f9f27a0da74
Athanasios-Mitroulakis <athanmit04@gmail.com>
2025-09-04 09:51:23 +0300

    Create sparse_constant_mul.cpp

---
+++ src/sparse_constant_mul.cpp
@@ -0,0 +1,22 @@
+#include "../inc/sparse.hpp"
+
+template <typename T>
+rixoratory::matrix_s<T>::matrix_s operator*(T x){
+    const MKL_INT n = this->rows*this->cols;
+    std::vector<T> n_vals(this->values); //_scal operates in place
+    if constexpr (std::is_same_v<T, float>)
+        sscal(n, x, n_vals.data(), 1);
+    else if constexpr (std::is_same_v<T, double>)
+        dscal(n, x, n_vals.data(), 1);
+    else if constexpr (std::is_same_v<T, std::complex<float>>)
+        cscal(n, &x, n_vals.data() 1);
+    else if constexpr (std::is_same_v<T, std::complex<double>>)
+        zscal(n, &x, n_vals.data() 1);
+    //reused code from scal_mn.cpp
+    return matrix_s<T>(n_vals, this->col_idx, this->row_idx, this->rows, this->cols);
+}
+
+template class matrix_s<float>;
+template class matrix_s<double>;
+template class matrix_s<std::complex<float>>;
+template class matrix_s<std::complex<double>>;
