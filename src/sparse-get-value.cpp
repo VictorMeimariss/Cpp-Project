02b6578631683ae8a5290ef8e8b146421fb734f3
Athanasios-Mitroulakis <athanmit04@gmail.com>
2025-09-22 17:29:36 +0300

    Create sparse-get-value.cpp

---
+++ src/sparse-get-value.cpp
@@ -0,0 +1,19 @@
+#include "../inc/sparse.hpp"
+template <typename T>
+T rixoratory::matrix_s::get_value_at(size_t index) const{
+    //index = c + col_size*r
+    size_t c = index%col_size;
+    size_t r = index/col_size;
+    size_t row_start = row_idx[r];
+    size_t row_end = row_idx[r+1];
+
+    for (int i = row_start; i<row_end; i++){
+        if (col_idx[i] == c)
+            return values[i]
+    }
+    return 0;
+}
+template class matrix_s<float>;
+template class matrix_s<double>;
+template class matrix_s<std::complex<float>>;
+template class matrix_s<std::complex<double>>;
