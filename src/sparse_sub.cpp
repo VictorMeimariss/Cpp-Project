54d067e7b7881c2bc675206b4d28bfa4c692e6e8
Athanasios-Mitroulakis <athanmit04@gmail.com>
2025-09-25 16:20:15 +0300

    Update sparse_sub.cpp
    updated namespace

--- src/sparse_sub.cpp
+++ src/sparse_sub.cpp
@@ -1,9 +1,9 @@
 #include "../inc/sparse.hpp"
 template <typename T>
-rixoratory::matrix_s<T> rixoratory::matrix_s<T>::operator-(matrix_s<T> const& A){
+sparse::matrix<T> sparse::matrix<T>::operator-(matrix<T> const& A){
     return *this+(A*(-1));
 }
-template class matrix_s<float>;
-template class matrix_s<double>;
-template class matrix_s<std::complex<float>>;
-template class matrix_s<std::complex<double>>;
+template class matrix<float>;
+template class matrix<double>;
+template class matrix<std::complex<float>>;
+template class matrix<std::complex<double>>;
