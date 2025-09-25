380e9be7957044909110e56cf3f8298da904168d
Athanasios-Mitroulakis <athanmit04@gmail.com>
2025-09-25 16:10:04 +0300

    Update sparse-diagonal-getter.cpp
    updated namespace

--- src/sparse-diagonal-getter.cpp
+++ src/sparse-diagonal-getter.cpp
@@ -1,12 +1,12 @@
 #include "../inc/sparse.hpp"
 template <typename T>
-const std::vector<T> rixoratory::matrix_s::diag() const{
+const std::vector<T> sparse::matrix::diag() const{
     std::vector<T> ret;
     for (int i = 0; i<col_size; i++)
         ret.push_back(this->get_value_at(i*col_size + i));
     return ret;
 }
-template class matrix_s<float>;
-template class matrix_s<double>;
-template class matrix_s<std::complex<float>>;
-template class matrix_s<std::complex<double>>;
+template class matrix<float>;
+template class matrix<double>;
+template class matrix<std::complex<float>>;
+template class matrix<std::complex<double>>;
