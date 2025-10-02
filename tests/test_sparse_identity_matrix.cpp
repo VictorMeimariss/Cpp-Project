0d7de966d4142e9dde7cec441e464b0b659e9311
johnzik <zgian04@gmail.com>
2025-10-03 00:15:32 +0300

    Sparse I matrix testing cpp created

---
+++ tests/test_sparse_identity_matrix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "../inc/sparse.hpp"
+using namespace sparse;
+
+int main()
+{
+    std::cout << "Testing Sparse Identity Matrix function" << std::endl;
+
+    int N = 5;
+    std::cout << "Sparse Identity Matrix size: " << N << std::endl;
+
+    matrix<float> A;
+    A.I(N);
+
+    
+}
