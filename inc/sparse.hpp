5455d8f0180139faaf94bce4e6a85c6de9da92c2
Athanasios-Mitroulakis <athanmit04@gmail.com>
2025-12-09 01:45:10 +0200

    Declare FGMRES_dsolve function in sparse.hpp

--- inc/sparse.hpp
+++ inc/sparse.hpp
@@ -1,5 +1,6 @@
 #ifndef SPARSE_HPP
 #define SPARSE_HPP
+#include <cstring>
 
 /**
  * @file sparse.hpp
@@ -261,6 +262,7 @@ matrix<T> operator*(U x, const matrix<T>& A); // Victor
  */
 template<typename T>
 std::vector<T> operator*(const std::vector<T> x, const matrix<T>& A); // Victor
-
 }
+std::vector<double> FGMRES_dsolve(sparse::matrix<double> & A, std::vector<double> & b, std::vector<double> & x0);
+
 #endif
