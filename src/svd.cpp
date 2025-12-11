7f92a4106cab30155a9e863f6c14e13cb2e51de3
Athanasios-Mitroulakis <athanmit04@gmail.com>
2025-12-11 21:25:38 +0200

    Change LAPACK job parameter from 'S' to 'A'

--- src/svd.cpp
+++ src/svd.cpp
@@ -16,21 +16,23 @@ void matrix<T>::svd(matrix<T>& u, matrix<T>& s, matrix<T>& v){
     T* A_data = (T*) mkl_malloc(mem1, 64);
     memcpy(A_data, values.data(), mem1);
 
+    std::cout<<m<<' '<<n<<std::endl;
+
     std::vector<T> u_vec(m*m, 0);
     std::vector<T> v_vec(n*n, 0);
     if constexpr (std::is_same_v<T, float>){
         std::vector<float> s_vec (n,0);
-        info = LAPACKE_sgesdd (LAPACK_ROW_MAJOR , 'S' , m, n , A_data , lda, s_vec.data(), u_vec.data(), ldu, v_vec.data(), ldv);
+        info = LAPACKE_sgesdd (LAPACK_ROW_MAJOR , 'A' , m, n , A_data , lda, s_vec.data(), u_vec.data(), ldu, v_vec.data(), ldv);
         s = matrix<float>(s_vec, m, n);
     }
     else if constexpr (std::is_same_v<T, double>){
         std::vector<double> s_vec (n,0);
-        info = LAPACKE_dgesdd (LAPACK_ROW_MAJOR , 'S' , m, n ,A_data, lda, s_vec.data(), u_vec.data(), ldu, v_vec.data(), ldv);
+        info = LAPACKE_dgesdd (LAPACK_ROW_MAJOR , 'A' , m, n ,A_data, lda, s_vec.data(), u_vec.data(), ldu, v_vec.data(), ldv);
         s = matrix<double>(s_vec, m, n);
     }
     else if constexpr (std::is_same_v<T, std::complex<float>>){
         std::vector<float> s_vec (n,0);
-        info = LAPACKE_cgesdd (LAPACK_ROW_MAJOR , 'S' , m, n , reinterpret_cast<MKL_Complex8*> (A_data) , lda, s_vec.data(), reinterpret_cast<MKL_Complex8*> (u_vec.data()), ldu, reinterpret_cast<MKL_Complex8*> (v_vec.data()), ldv);
+        info = LAPACKE_cgesdd (LAPACK_ROW_MAJOR , 'A' , m, n , reinterpret_cast<MKL_Complex8*> (A_data) , lda, s_vec.data(), reinterpret_cast<MKL_Complex8*> (u_vec.data()), ldu, reinterpret_cast<MKL_Complex8*> (v_vec.data()), ldv);
         std::vector<std::complex<float>> s_vec_c;
         for(auto i: s_vec){
             s_vec_c.push_back(std::complex<float>(i,0));
@@ -39,14 +41,13 @@ void matrix<T>::svd(matrix<T>& u, matrix<T>& s, matrix<T>& v){
     }
     else if constexpr (std::is_same_v<T, std::complex<double>>){
         std::vector<double> s_vec (n,0);
-        info = LAPACKE_zgesdd (LAPACK_ROW_MAJOR , 'S' , m, n , reinterpret_cast<MKL_Complex16*> (A_data) , lda, s_vec.data(), reinterpret_cast<MKL_Complex16*> (u_vec.data()), ldu, reinterpret_cast<MKL_Complex16*> (v_vec.data()), ldv);
+        info = LAPACKE_zgesdd (LAPACK_ROW_MAJOR , 'A' , m, n , reinterpret_cast<MKL_Complex16*> (A_data) , lda, s_vec.data(), reinterpret_cast<MKL_Complex16*> (u_vec.data()), ldu, reinterpret_cast<MKL_Complex16*> (v_vec.data()), ldv);
         std::vector<std::complex<double>> s_vec_z;
         for(auto i: s_vec){
             s_vec_z.push_back(std::complex<double>(i,0));
         }
         s = matrix<std::complex<double>>(s_vec_z, m, n);
     }
-
     u = matrix<T>(u_vec, std::vector<size_t>{(size_t)(m),(size_t)(m)}, std::vector<int>{m, 1});
     v = matrix<T>(v_vec, std::vector<size_t>{(size_t)(n),(size_t)(n)}, std::vector<int>{n, 1});
 }
