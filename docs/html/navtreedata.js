583d306b5c4148aa55018b94450bb1cf33dc0489
johnzik <zgian04@gmail.com>
2026-03-04 21:34:09 +0200

    Added git submodule to modernize html appearance

--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -24,14 +24,8 @@
 */
 var NAVTREE =
 [
-  [ "Dense and CSR Sparse matrix library in C++ leveraging Intel oneMKL (CBLAS)", "index.html", [
-    [ "What this project does", "index.html#autotoc_md1", null ],
-    [ "How to build & compile the project", "index.html#autotoc_md2", [
-      [ "Requirements", "index.html#autotoc_md3", null ],
-      [ "Setup Instructions", "index.html#autotoc_md4", null ],
-      [ "Build & Linking Instructions", "index.html#autotoc_md5", null ],
-      [ "Run a test (optional)", "index.html#autotoc_md6", null ]
-    ] ],
+  [ "CPP-PROJECT", "index.html", [
+    [ "Dense and CSR Sparse matrix library in C++ leveraging Intel oneMKL (CBLAS)", "index.html", "index" ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Members", "functions.html", [
